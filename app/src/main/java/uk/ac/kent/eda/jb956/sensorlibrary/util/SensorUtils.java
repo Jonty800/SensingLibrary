8b00ed3a2f1ad4211c9e61a0feffdc52da286814
Jon Baker <jonty800@gmail.com>
2017-09-13 11:31:31 +0100

    Missing SensorUtils.java

---
+++ app/src/main/java/uk/ac/kent/eda/jb956/sensorlibrary/util/SensorUtils.java
@@ -0,0 +1,21 @@
+package uk.ac.kent.eda.jb956.sensorlibrary.util;
+
+/**
+ * Copyright (c) 2017, Jon Baker <Jonty800@gmail.com>
+ * School of Engineering and Digital Arts, University of Kent
+ */
+
+public class SensorUtils {
+    public final static int SENSOR_TYPE_ACCELEROMETER = 5001;
+    public final static int SENSOR_TYPE_LOCATION = 5002;
+    public final static int SENSOR_TYPE_MICROPHONE = 5003;
+    public final static int SENSOR_TYPE_PROXIMITY = 5004;
+    public final static int SENSOR_TYPE_WIFI = 5005;
+    public final static int SENSOR_TYPE_GYROSCOPE = 5006;
+    public final static int SENSOR_TYPE_LIGHT = 5007;
+    public final static int SENSOR_TYPE_AMBIENT_TEMPERATURE = 5008;
+    public final static int SENSOR_TYPE_PRESSURE = 5009;
+    public final static int SENSOR_TYPE_HUMIDITY = 5010;
+    public final static int SENSOR_TYPE_MAGNETIC_FIELD = 5011;
+    public final static int SENSOR_TYPE_ACTIVITY = 5012;
+}
