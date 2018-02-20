7e043b52ca24ab21cbb342114f150821d816e5f5
Jon Baker <jonty800@gmail.com>
2018-02-20 20:21:14 +0000

    NTP is no longer static

--- app/src/main/java/uk/ac/kent/eda/jb956/sensorlibrary/util/NTP.java
+++ app/src/main/java/uk/ac/kent/eda/jb956/sensorlibrary/util/NTP.java
@@ -2,21 +2,33 @@ package uk.ac.kent.eda.jb956.sensorlibrary.util;
 
 import android.os.SystemClock;
 
-        import android.os.SystemClock;
-
 /**
  * Copyright (c) 2017, Jon Baker <Jonty800@gmail.com>
  * School of Engineering and Digital Arts, University of Kent
  */
 
 public class NTP {
-    private static long real_time = 0;
-    private static long offset = 0;
-    private static boolean ahead = true;
-    private static long lastTimeSync = 0L;
-    public static synchronized long currentTimeMillis(){
+    private long real_time = 0L;
+    private long offset = 0L;
+    private boolean ahead = true;
+    private long lastTimeSync = 0L;
+    private int interval = 120_000;
+
+    public NTP getInstance() {
+        if(instance == null)
+            instance = new NTP();
+        return instance;
+    }
+
+    private NTP instance;
+
+    public void setUpdateInterval(int intervalInMilliseconds){
+        interval = intervalInMilliseconds;
+    }
+
+    public synchronized long currentTimeMillis(){
         SntpClient client = new SntpClient();
-        if (real_time == 0 && client.requestTime("pool.ntp.org", 3000)) {
+        if (real_time == 0L && client.requestTime("pool.ntp.org", 3000)) {
             real_time = client.getNtpTime() + SystemClock.elapsedRealtime() - client.getNtpTimeReference();
             long time = System.currentTimeMillis();
             offset = Math.abs(real_time-time);
@@ -33,6 +45,7 @@ public class NTP {
             System.out.println("Timestamp Sync Results: Offset=" + offset + " ahead="+ahead + " actual_ts=" + real_time + " old_ts=" + time + " new_ts="+test );
         }else{
             //server failed. Dont set real_time and just return currentTimeMillis
+            System.out.println("Unable to get time, returning System.currentTimeMillis()");
             return System.currentTimeMillis();
         }
         long adjustedTimestamp;
@@ -43,8 +56,8 @@ public class NTP {
             //remove the missing time
             adjustedTimestamp = System.currentTimeMillis() - offset;
         }
-        if(lastTimeSync != 0 && Math.abs(lastTimeSync - adjustedTimestamp) > (60000 * 2))
-            real_time = 0;
+        if(lastTimeSync != 0L && Math.abs(lastTimeSync - adjustedTimestamp) > interval)
+            real_time = 0L;
         return adjustedTimestamp;
     }
 }
